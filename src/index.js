8adc0af01237ce60e2c1dd7a3b4e4281894b126d
Francisco Roa Prieto <F.Prieto@sofort.com>
2017-08-08 12:21:41 +0200

    Added testing configuration

--- src/index.js
+++
@@ -1,7 +0,0 @@
-require('./assets/stylesheets/styles.scss');
-
-import React from 'react'; // import the main react dependency
-import ReactDOM from 'react-dom'; // import reactDOM
-import App from './app/App.jsx'; // import the main app component
-
-ReactDOM.render(<App />, document.getElementById('root')); // render our App component and mount it to our #root element
